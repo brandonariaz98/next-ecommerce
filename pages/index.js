84b22092c853a6c6d19518e436a09de5ba3fe0f8
Brandon Arias <2017030706@upsin.edu.mx>
2024-10-28 21:11:19 -0700

    Product Add, Update and Delete

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,11 @@ export default function Home() {
   return (
     <Layout>
       <div className="text-blue-900 flex justify-between">
-        <h2>Hello, <b>{session.user.name}</b></h2>
+        <h2>Hello, <b>{session?.user?.name}</b></h2>
         <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
-          <img src={session.user.image} alt="" className="w-6 h-6" />
+          <img src={session?.user?.image} alt="" className="w-6 h-6" />
           <span className="px-2">
-            {session.user.name}
+            {session?.user?.name}
           </span>
         </div>
       </div>
