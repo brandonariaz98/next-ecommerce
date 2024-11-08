a86963d87151d8708088a7fc1dd51e29d616296d
Brandon Arias <2017030706@upsin.edu.mx>
2024-11-07 21:58:28 -0700

    Admin mobile responsive added

--- pages/categories.js
+++ pages/categories.js
@@ -162,7 +162,7 @@ function Categories({ swal }) {
                 <button
                   onClick={() => removeProperty(index)}
                   type="button"
-                  className="btn-default"
+                  className="btn-red"
                 >
                   Remove
                 </button>
@@ -208,13 +208,13 @@ function Categories({ swal }) {
                   <td>
                     <button
                       onClick={() => editCategory(category)}
-                      className="btn-primary mr-1"
+                      className="btn-default mr-1"
                     >
                       Edit
                     </button>
                     <button
                       onClick={() => deleteCategory(category)}
-                      className="btn-primary"
+                      className="btn-red"
                     >
                       Delete
                     </button>
